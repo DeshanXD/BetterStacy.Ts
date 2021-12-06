70b4bbce1990a20ae09c2a4dd455485fc91a06f4
deshanbasu <deshanbasura3@gmail.com>
2021-12-07 03:32:20 +0530

    Modified Bot Class

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,21 +1,42 @@
 import consola, { Consola } from "consola";
 import { Config } from "./interfaces/Config";
-import { Client } from "discord.js";
+import { Client, Collection, Intents } from "discord.js";
+import { Command } from "./interfaces/Command";
+import { Event } from "./interfaces/Event";
+
+import EventEmitter from "events";
+import glob from "glob";
+import { promisify } from "util";
+
+const globPromise = promisify(glob);
 
 class BetterStacy extends Client {
   public logger: Consola = consola;
   public config: Config;
   public prefix: string;
+  public commands: Collection<string, Command> = new Collection();
+  public aliases: Collection<string, string> = new Collection();
+  public cooldowns: Collection<string, number> = new Collection();
+  public categories: Set<string> = new Set();
 
   public constructor() {
     // calling the constructor
-    super({ intents: 32767 });
+    // super({ intents: 32767 });
+    super({
+      intents: [
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILDS,
+      ],
+    });
   }
 
   public async start(config: Config) {
     this.config = config;
     this.prefix = config.prefix;
 
+    console.log(config);
     // this.registry.registerCommandsIn(path.join(__dirname, "commands"));
 
     // new WOKCommands(this, {
@@ -23,13 +44,42 @@ class BetterStacy extends Client {
     //   typeScript: true,
     // });
 
-    this.on("ready", () => {
+    this.on("interactionCreate", () => {
       this.logger.info(
-        `${this.user.username} is online on ${this.guilds.cache.size} servers!`
+        // `${this.user.username} is online on ${this.guilds.cache.size} servers!`
+        "Interaction is created"
       );
     });
 
     this.login(config.token).catch((e) => this.logger.error(e));
+
+    const commandFiles: string[] = await globPromise(
+      `${__dirname}/commands/*{.js,.ts}`
+    );
+    const eventFiles: string[] = await globPromise(
+      `${__dirname}/events/*{.js,.ts}`
+    );
+
+    commandFiles.map(async (cmdFile: string) => {
+      const cmd = (await import(cmdFile)) as Command;
+      this.commands.set(cmd.name, { cooldown: 3000, ...cmd });
+      this.categories.add(cmd.category);
+    });
+
+    eventFiles.map(async (eventFile: string) => {
+      const event = (await import(eventFile)) as Event;
+      if (event.emitter && typeof event.emitter == "function") {
+        event.emitter(this).on(event.name, event.run.bind(null, this));
+      } else if (event.emitter && event.emitter instanceof EventEmitter) {
+        (event.emitter as EventEmitter).on(
+          event.name,
+          event.run.bind(null, this)
+        );
+      } else {
+        console.log(event.name);
+        this.on(event.name, event.run.bind(null, this));
+      }
+    });
   }
 }
 
