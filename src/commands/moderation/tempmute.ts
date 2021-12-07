3ff2fd8dd8fffe4edfa2f89d304cf14e38ef861c
deshanbasu <deshanbasura3@gmail.com>
2021-12-07 18:17:01 +0530

    :bug: Bug fixed

--- src/commands/moderation/tempmute.ts
+++ src/commands/moderation/tempmute.ts
@@ -19,15 +19,18 @@ export const command: Command = {
     if (!guilduserObject)
       return await message.reply("You need to mention someone to mute!");
 
+    if (!guilduserObject.voice.channel)
+      return await message.reply(
+        "You can only mute people who are in voice channels!"
+      );
+
     const votemuteEmbed = new MessageEmbed()
       .setColor("#0099ff")
       .setAuthor(
         `${guilduserObject.user.username}`,
         `${guilduserObject.user.avatarURL()}`
       )
-      .setDescription(
-        `You are getting vote muted by people on channel | ${message.member.voice.channel.name} | initiated by ${message.author} `
-      );
+      .setDescription(`You are getting vote muted by ${message.author} `);
 
     const editedVotMuteEmbed = new MessageEmbed()
       .setColor("#0099ff")
@@ -35,7 +38,7 @@ export const command: Command = {
         `${guilduserObject.user.username}`,
         `${guilduserObject.user.avatarURL()}`
       )
-      .setDescription(`You got vote muted by !tempmute for 45 seconds`);
+      .setDescription(`You got vote muted by !tempmute for 1 minute`);
 
     try {
       let sendMessage = await message.channel.send({ embeds: [votemuteEmbed] });
@@ -50,17 +53,18 @@ export const command: Command = {
         .then((collected) => {
           if (collected.size >= 1) {
             guilduserObject.voice.setMute(true);
+            console.log(`Muted ${guilduserObject.user.tag}`);
             sendMessage.edit({ embeds: [editedVotMuteEmbed] });
 
             // unmutes user
             setTimeout(() => {
               guilduserObject.voice.setMute(false);
-              console.log(`Unmuted ${guilduserObject.user}`);
-            }, 45_000);
+              console.log(`Unmuted ${guilduserObject.user.tag}`);
+            }, 60_000);
 
             message.delete();
           } else {
-            message.delete();
+            message.reply("You need at least 5 people to vote mute person");
             sendMessage.delete();
           }
         });
