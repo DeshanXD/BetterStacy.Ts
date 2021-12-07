e0bf7073539b03057efcb3bc0f58fd1676da88d1
deshanbasu <deshanbasura3@gmail.com>
2021-12-07 17:30:53 +0530

    Added GUILD_MESSAGE_REACTIONS Intent

--- src/Bot.ts
+++ src/Bot.ts
@@ -21,6 +21,7 @@ class BetterStacy extends Client {
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_PRESENCES,
         Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILDS,
       ],
     });
