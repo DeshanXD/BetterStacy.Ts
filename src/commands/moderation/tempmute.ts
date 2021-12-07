08a3c3403395278d80502be51bd9845d1493b7e5
deshanbasu <deshanbasura3@gmail.com>
2021-12-07 17:28:03 +0530

    Implemented original tempmute logic with some modifications

--- src/commands/moderation/tempmute.ts
+++ src/commands/moderation/tempmute.ts
@@ -16,6 +16,8 @@ export const command: Command = {
   run: async (client, message, args) => {
     // working on GuildUserObject
     const guilduserObject: GuildMember = message.mentions.members.first();
+    if (!guilduserObject)
+      return await message.reply("You need to mention someone to mute!");
 
     const votemuteEmbed = new MessageEmbed()
       .setColor("#0099ff")
@@ -33,27 +35,37 @@ export const command: Command = {
         `${guilduserObject.user.username}`,
         `${guilduserObject.user.avatarURL()}`
       )
-      .setDescription(
-        `You got vote muted by ${message.member.voice.channel.name} chat using !tempmute`
-      );
+      .setDescription(`You got vote muted by !tempmute for 45 seconds`);
 
     try {
-      if (!message.mentions.members.first())
-        return await message.reply("You need to mention someone to mute!");
-
       let sendMessage = await message.channel.send({ embeds: [votemuteEmbed] });
-      // let reactionFilter = (reaction: MessageReaction, user: User) => reaction.emoji.name === '✅'
       await sendMessage.react("✅");
 
-      await sendMessage.awaitReactions({
-        filter: (reaction: MessageReaction, user: User) =>
-          reaction.emoji.name === "✅",
-        time: 10_000,
-      }).then(collected => if(collected.size >= 5 ) );
+      await sendMessage
+        .awaitReactions({
+          filter: (reaction: MessageReaction, user: User) =>
+            reaction.emoji.name === "✅",
+          time: 15_000,
+        })
+        .then((collected) => {
+          if (collected.size >= 1) {
+            guilduserObject.voice.setMute(true);
+            sendMessage.edit({ embeds: [editedVotMuteEmbed] });
+
+            // unmutes user
+            setTimeout(() => {
+              guilduserObject.voice.setMute(false);
+              console.log(`Unmuted ${guilduserObject.user}`);
+            }, 45_000);
+
+            message.delete();
+          } else {
+            message.delete();
+            sendMessage.delete();
+          }
+        });
     } catch (error) {
       console.log(error);
     }
   },
 };
-
-function userMute(member: GuildMember) {}
