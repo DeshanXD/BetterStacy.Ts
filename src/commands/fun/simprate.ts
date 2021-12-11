3e18a17734e8540c27ba3331d13fbe58ddbc161f
Deshan Basura <basuradeshan@gmail.com>
2021-12-11 08:42:12 +0530

    Modified Commands and Events
    Added inviteCreate event: Not initialized yet

--- src/commands/fun/simprate.ts
+++ src/commands/fun/simprate.ts
@@ -11,6 +11,30 @@ export const command: Command = {
 
     await message.reply(`You are ${rate}% simp in the simpverse!`);
 
+    await client.knex
+      .select("discord_id")
+      .from("users")
+      .where("discord_id", user.id)
+      .then((userNametList) => {
+        if (userNametList.length === 0) {
+          return client
+            .knex("users")
+            .insert([
+              {
+                discord_id: user.id,
+                coins: rate,
+              },
+            ])
+            .then((newUserId) => {
+              console.log("inserted user", newUserId);
+            });
+        }
+        console.log("not inserting user");
+        return;
+      });
+
+    // await client.knex("users").insert({ discord_id: user.id, coins: rate });
+
     // if (typeof rate === "number") {
     await client.cache.set(user.id, `${rate}`);
     // await client.redisClient.expire(user.id, 600);
