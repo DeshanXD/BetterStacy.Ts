e4a25b4da1389d9593c4ee1d768c06b596326cec
Deshan Basura <basuradeshan@gmail.com>
2021-12-12 23:18:04 +0530

    Fixed Deployment

--- src/commands/fun/pingsme.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../../interfaces/Command";
-
-export const command: Command = {
-  name: "pingsme",
-  aliases: ["pme"],
-  run: async (client, message, args) => {
-    message.channel.send(`${client.ws.ping} ping!`);
-  },
-};
