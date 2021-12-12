1d1f6bb11448e0dc19fb3c1f5bd50b3fcd758948
Deshan Basura <basuradeshan@gmail.com>
2021-12-12 22:22:36 +0530

    🐛 FIX: Simprate databse error

--- src/commands/fun/reddit.ts
+++ src/commands/fun/reddit.ts
@@ -4,6 +4,9 @@ export const command: Command = {
   name: "reddit",
   aliases: ["r", "red"],
   run: async (client, message, args) => {
+    console.log(args);
+    console.log(args[0]);
+
     if (message.type !== "REPLY")
       return await message.reply(
         "You need to reply to the image that you are trying to post!"
@@ -11,7 +14,7 @@ export const command: Command = {
 
     if (args[0].toLowerCase() !== "post")
       return await message.reply(
-        `You can only use "${client.prefix}reddit post" command which post an image to RDXGaming subreddit!"`
+        `You can only udiscord_id "${client.prefix}reddit post" command which post an image to RDXGaming subreddit!"`
       );
 
     await message.channel.send(
