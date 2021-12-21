d7670e85fbe02ee33aff530133af5f1680dcc93a
Deshan Basura <basuradeshan@gmail.com>
2021-12-21 12:09:25 +0530

    :+1: Finished guildBanRemove

--- src/events/guildBanRemove.ts
+++ src/events/guildBanRemove.ts
@@ -1,9 +1,29 @@
 import { Event } from "../interfaces/Event";
 import { Command } from "../interfaces/Command";
-import { User } from "discord.js";
+import { TextChannel, User } from "discord.js";
 import { BetterStacy } from "../Bot";
 
 export const event: Event = {
   name: "guildBanRemove",
-  run: (client: BetterStacy, guildId: string, user: User) => {},
+  run: async (client: BetterStacy, guildId: string, user: User) => {
+    let channel = client.guilds.cache
+      .get(guildId)
+      .channels.cache.find(
+        (channel) => channel.name === "general"
+      ) as TextChannel;
+    if (!!channel) {
+      let invite = await channel.createInvite({
+        maxUses: 1,
+      });
+
+      try {
+        user.send(
+          invite.url +
+            "\nYou have been unbanned from this server you can join now!"
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
 };
